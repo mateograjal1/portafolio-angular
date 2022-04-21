75276e269671cc6bf97c5baaf26731532f9b18ad
Mateo Grajales <mateo.grajales@o-i.com>
2022-04-21 15:42:24 -0500

    firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -3,23 +3,34 @@ import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InfoPaginaService {
-
   info: InfoPagina = {};
   cargada: boolean = false;
+  equipo: any[] = [];
 
-  constructor(private http:HttpClient) {
-    console.log("Servicio de info pagina cargado");
+  constructor(private http: HttpClient) {
+    console.log('Servicio de info pagina cargado');
+    this.cargaInfo();
+    this.cargarEquipo();
+  }
 
-    //Leer el archivo json
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
+  private cargaInfo() {
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
+      });
+  }
+
+  private cargarEquipo(){
+    this.http
+      .get('https://angular-html-a0649-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp;
         console.log(resp);
-      }
-      );
-   }
+      });
+  }
 }
